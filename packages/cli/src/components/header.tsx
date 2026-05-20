023615fd2ce58541d7a0e7537afb95cbf820bd09
dev-visiongenius <devloper.visiongenius@gmail.com>
2026-05-20 15:29:33 +0500

    added header

---
+++ packages/cli/src/components/header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <box justifyContent="center" alignItems="center">
+            <box flexDirection="row" justifyContent="center" alignItems="center" gap={0.5}>
+                <ascii-font font="tiny" text="Night" color={"gray"} />
+                <ascii-font font="tiny" text="Code" />
+            </box>
+        </box>
+    );
+};
+
+export default Header;
